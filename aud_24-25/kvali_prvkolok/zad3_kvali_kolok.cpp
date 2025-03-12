aade27c7f37be93cca782c1321a6bc2713ac83a3
Donik Goxha <donik.godzha@students.finki.ukim.mk>
2025-03-12 16:16:06 +0100

    add

--- aud_24-25/kvali_prvkolok/zad3_kvali_kolok.cpp
+++ aud_24-25/kvali_prvkolok/zad3_kvali_kolok.cpp
@@ -1,3 +1,64 @@
-//
-// Created by Donik Goxha on 11-Mar-25.
-//
+#include <iostream>
+using namespace std;
+
+class Temperature {
+private:
+    float temp;
+
+public:
+    Temperature() {
+        temp = 0;
+    }
+
+    Temperature(float tempp) {
+        temp = tempp;
+    }
+
+    float get() const {
+        return temp;
+    }
+
+    void changeTemp() {
+        if (temp >= 0) {
+            temp++;
+        } else {
+            temp--;
+        }
+    }
+};
+
+
+int main() {
+    Temperature obj1;
+    int n;
+    cin >> n;
+    int cmd;
+    float k;
+    for (int i = 0; i < n; i++) {
+        cin >> cmd;
+        switch (cmd) {
+            case 1: {
+                cin >> k;
+                Temperature obj2(k);
+                cout << obj2.get() << endl;
+                break;
+            }
+            case 2: {
+                obj1.changeTemp();
+                cout << obj1.get() << endl;
+                break;
+            }
+            case 3: {
+                cin >> k;
+                Temperature obj3(k);
+                obj3.changeTemp();
+                cout << obj3.get() << endl;
+                break;
+            }
+            default: {
+                cout << obj1.get() << endl;
+                break;
+            }
+        }
+    }
+}
--- aud_24-25/kvali_prvkolok/zad5_kvali_kolok.cpp
+++ aud_24-25/kvali_prvkolok/zad5_kvali_kolok.cpp
@@ -1,3 +1,63 @@
-//
-// Created by Donik Goxha on 11-Mar-25.
-//
+#include <iostream>
+using namespace std;
+
+class Letter {
+private:
+    char ch;
+
+public:
+    Letter() {
+        ch = 'a';
+    }
+
+    Letter(char c) {
+        ch = c;
+    }
+
+    char get() const {
+        return ch;
+    }
+
+    void upperLetter() {
+        if (isalpha(ch)) {
+            ch = toupper(ch);
+        } else {
+            cout << "Character is not a letter" << endl;
+        }
+    }
+};
+
+int main() {
+    Letter obj;
+    int n;
+    cin >> n;
+    int cmd;
+    char k;
+    for (int i = 0; i < n; i++) {
+        cin >> cmd;
+        switch (cmd) {
+            case 1: {
+                cin >> k;
+                obj = Letter(k);
+                cout << obj.get() << endl;
+                break;
+            }
+            case 2: {
+                obj.upperLetter();
+                cout << obj.get() << endl;
+                break;
+            }
+            case 3: {
+                cin >> k;
+                Letter obj2(k);
+                obj2.upperLetter();
+                cout << obj2.get() << endl;
+                break;
+            }
+            default: {
+                cout << obj.get() << endl;
+                break;
+            }
+        }
+    }
+}
